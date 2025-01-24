323fce36893f3f8a820ef72290a285e41eb621d1
root <root@LAPTOP-33N12T6F.>
2025-01-24 09:17:46 -0500

    addition of two numbers

--- addition_and_multipilication_of_two_numbers.sh
+++ addition_and_multipilication_of_two_numbers.sh
@@ -3,7 +3,7 @@
 Value_1=$1
 Value_2=$2
 
-# calculation the two numbers sum and multiplication and print it
+# calculation the two numbers sum  and print it
 
 echo $((Value_1 + Value_2))
-echo $((Value_1 * Value_2)) 
+
---
+++ addition_and_multipilication_of_two_numbers_correctly.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#calculate these numbers for the resuly
+RESULT=$(./addition_and_multipilication_of_two_numbers.sh 10 5)
+
+echo "result can be calculated: $Result"
+
+#statements
+if [[ $RESULT -eq 15 ]]; then
+   echo "result is 15"
+else
+   echo "error"
+fi
+
